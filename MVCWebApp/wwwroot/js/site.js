380d104111348bbadb991ef6126139c55449dc22
Abdul_cj53 <Abdul_cj53@STHQ0118-05>
2024-04-03 11:55:09 +0400

    Add project files.

---
+++ MVCWebApp/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿function showPdf(fileUrl) {
+    //var pdfViewer = document.getElementById('pdfViewer');
+    pdfViewer.src = fileUrl;
+    pdfViewer.style.display = 'block';
+    PDFObject.embed(pdfUrl, 'pdfViewer');
+}
+
+
+
+//function showPdf(pdfUrl) {
+//    var xhr = new XMLHttpRequest();
+//    xhr.open('GET', pdfUrl, true);
+//    xhr.responseType = 'blob';
+//    xhr.onload = function () {
+//        if (this.status === 200) {
+//            var blob = new Blob([this.response], { type: 'application/pdf' });
+//            var objectUrl = URL.createObjectURL(blob);
+//            var pdfViewer = document.getElementById('pdfViewer');
+//            pdfViewer.src = objectUrl;
+//            pdfViewer.style.display = 'block';
+//        }
+//    };
+//    xhr.send();
+//}
